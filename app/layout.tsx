c313424b326fb7d21291363b97eebf5518252fc0
gnoni17 <gnonigabriele@gmail.com>
2024-10-03 11:21:36 +0200

    fix: import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Metadata } from 'next';
  
-import { Metadata } from 'next';
- 
 export const metadata: Metadata = {
   title: {
     template: '%s | Acme Dashboard',
